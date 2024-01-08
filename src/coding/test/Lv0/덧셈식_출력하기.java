e19f4561535f1290864bccab762e25a178e2d250
YangHyeGyo <gpry4618@naver.com>
2024-01-08 13:39:05 +0900

    덧셈식 출력하기

---
+++ src/coding/test/Lv0/덧셈식_출력하기.java
@@ -0,0 +1,13 @@
+package coding.test.Lv0;
+// 덧셈식 출력하기
+import java.util.Scanner;
+
+public class 덧셈식_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a+b));
+    }
+}
