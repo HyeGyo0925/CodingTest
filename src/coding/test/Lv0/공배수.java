64a412cf83dc52e924d322fff4ac4298e82efe92
YangHyeGyo <gpry4618@naver.com>
2024-01-09 09:29:14 +0900

    공배수

---
+++ src/coding/test/Lv0/공배수.java
@@ -0,0 +1,12 @@
+package coding.test.Lv0;
+//공배수
+
+public class 공배수 {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if(number % n ==0 && number % m == 0){
+            answer=1;
+        }else answer=0;
+        return answer;
+    }
+}
