d42b44ceed1d8243ee715b47d65b3dcc05815aa2
YangHyeGyo <gpry4618@naver.com>
2024-01-10 10:03:43 +0900

    코드_처리하기2

--- src/coding/test/Lv0/코드_처리하기.java
+++ src/coding/test/Lv0/코드_처리하기.java
@@ -3,35 +3,22 @@ package coding.test.Lv0;
 
 public class 코드_처리하기 {
     public String solution(String code){
-        String answer="";
+        StringBuilder answer = new StringBuilder();
         int mode = 0;
-        for(int i=0; i<code.length(); i++){
-            String s = code.substring(i, i+1);
 
-            if(mode == 0){
-                if(s.equals("1")){
-                    mode = 1;
-                }else{
-                    if(i%2==0){
-                        answer += s;
-                    }
-                }
-            }else{
-                if(s.equals("1")){
-                    mode = 0;
-                }else{
-                    if(i%2==1){
-                        answer += s;
-                    }
-                }
+        for(int i=0; i<code.length(); i++){
+            char current = code.charAt(i);
+            if(current == '1'){
+                mode = mode == 0 ? 1 : 0;
+                continue;
             }
 
+            if(i % 2 == mode){
+                answer.append(current);
+            }
         }
 
-        if(answer.equals("")){
-            answer = "EMPTY";
-        }
-        return answer;
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
     }
 
     public static void main(String[] args) {
