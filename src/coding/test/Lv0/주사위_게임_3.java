3787e5256501bf6badbdc2b26c63c5023c382e4d
YangHyeGyo <gpry4618@naver.com>
2024-01-15 14:55:17 +0900

    주사위 게임 3_3

--- src/coding/test/Lv0/주사위_게임_3.java
+++ src/coding/test/Lv0/주사위_게임_3.java
@@ -5,43 +5,32 @@ import java.util.Arrays;
 
 public class 주사위_게임_3 {
     public int solution(int a, int b, int c, int d) {
-        int answer = 0;
-        if(a==b && b==c && c==d){
-            answer = 1111 * a;
-        } else if (a==b && b==c) {
-            answer = ((10*a+d) * (10*a+d));
-        } else if (a == c && c == d) {
-            answer =  ((10 * a + b) * (10 * a + b));
-        } else if (a == b && b == d) {
-            answer =  ((10 * a + c) * (10 * a + c));
-        } else if (b == c && c == d) {
-            answer =  ((10 * b + a) * (10 * b + a));
-        } else if (a == b && c == d) {
-            answer =  ((a + c) * (Math.abs(a - c)));
-        } else if (a == c && b == d) {
-            answer =  ((a + b) * (Math.abs(a - b)));
-        } else if (a == d && b == c) {
-            answer =  ((a + b) * (Math.abs(a - b)));
-        } else if (a == b && c != d) {
-            answer =  (c * d);
-        } else if (a == c && b != d) {
-            answer =  (b * d);
-        } else if (a == d && b != c) {
-            answer =  (b * c);
-        } else if (b == c && a != d) {
-            answer =  (a * d);
-        } else if (b == d && a != c) {
-            answer =  (a * c);
-        } else if (c == d && a != b) {
-            answer =  (a * b);
+        int[] dice = { a, b, c, d };
+        Arrays.sort(dice);
+
+        int ans = 0;
+
+        if (dice[0] == dice[3]) {
+            ans = 1111 * dice[3];
+        } else if (dice[0] == dice[2] || dice[1] == dice[3]) {
+            ans = (int) Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]), 2);
+        } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
+            ans = (dice[0] + dice[3]) * (dice[3] - dice[0]);
+        } else if (dice[0] == dice[1]) {
+            ans = dice[2] * dice[3];
+        } else if (dice[1] == dice[2]) {
+            ans = dice[0] * dice[3];
+        } else if (dice[2] == dice[3]) {
+            ans = dice[0] * dice[1];
         } else {
-            answer =  Math.min(Math.min(a, b), Math.min(c, d));
+            ans = dice[0];
         }
-        return answer;
+
+        return ans;
     }
 
     public static void main(String[] args) {
         주사위_게임_3 aa = new 주사위_게임_3();
-        System.out.println(aa.solution(2,2,2,1));
+        System.out.println(aa.solution(4,1,4,4));
     }
 }
