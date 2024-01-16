2f658acc96eba989dd86210374942ba93b2ca952
YangHyeGyo <gpry4618@naver.com>
2024-01-16 14:38:48 +0900

    접미사인지 확인하기2

--- src/coding/test/Lv0/접미사인지_확인하기.java
+++ src/coding/test/Lv0/접미사인지_확인하기.java
@@ -2,19 +2,11 @@ package coding.test.Lv0;
 
 public class 접미사인지_확인하기 {
     public int solution(String my_string, String is_suffix) {
-        int answer = 0;
-        String[] test = new String[my_string.length()];
-        for(int i=0; i<my_string.length(); i++){
-            test[i] = my_string.substring(i);
+        if(my_string.endsWith(is_suffix)) {
+            return 1;
+        } else {
+            return 0;
         }
-
-        for(int i=0; i<my_string.length(); i++){
-            if(test[i].equals(is_suffix)){
-                answer = 1;
-                break;
-            }else answer = 0;
-        }
-        return answer;
     }
 
     public static void main(String[] args) {
