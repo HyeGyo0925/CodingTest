a9dd4e51d7c676742304a8fff6f246c03529906a
YangHyeGyo <gpry4618@naver.com>
2024-01-23 13:08:56 +0900

    정수 내림차순으로 배치하기2

--- src/coding/test/Lv1/정수_내림차순으로_배치하기.java
+++ src/coding/test/Lv1/정수_내림차순으로_배치하기.java
@@ -4,27 +4,13 @@ import java.util.Arrays;
 
 public class 정수_내림차순으로_배치하기 {
     public long solution(long n) {
-        String answer = "";
-        String s = Long.toString(n);
-        int[] test = new int[s.length()];
-        for(int i=0; i<s.length(); i++){
-            test[i] = Integer.parseInt(String.valueOf(s.charAt(i)));
-        }
+        String[] list = String.valueOf(n).split("");
+        Arrays.sort(list);
 
-        for(int i=0; i<s.length()-1; i++){
-            for(int j=0; j<s.length()-1; j++){
-                if(test[j] < test[j+1]){
-                    int tmp = test[j+1];
-                    test[j+1] = test[j];
-                    test[j] = tmp;
-                }
-            }
-        }
+        StringBuilder sb = new StringBuilder();
+        for(String aList : list) sb.append(aList);
 
-        for(int i=0; i<s.length(); i++){
-            answer += test[i];
-        }
-        return Long.parseLong(String.valueOf(answer));
+        return Long.parseLong(sb.reverse().toString());
     }
 
     public static void main(String[] args) {
