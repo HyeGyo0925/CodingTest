e9d22f080a51d043262a4b28528e03a8d7f7882a
YangHyeGyo <gpry4618@naver.com>
2024-01-30 09:23:45 +0900

    3진법 뒤집기2

--- src/coding/test/Lv1/삼진법_뒤집기.java
+++ src/coding/test/Lv1/삼진법_뒤집기.java
@@ -8,13 +8,29 @@ public class 삼진법_뒤집기 {
             str += n%3;
             n/=3;
         }
-
         return  Integer.parseInt(str, 3);
+    }
+
+    public int solution1(int n){
+        int answer=0;
+        String str = "";
+
+        while(n>0){
+            str = n%3 + str;
+            n/=3;
+        }
+
+        for(int i=0; i<str.length(); i++){
+            answer += Math.pow(3, i) * (str.charAt(i) - '0');
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
         삼진법_뒤집기 aa = new 삼진법_뒤집기();
         System.out.println(aa.solution(45));
+        System.out.println(aa.solution1(45));
        // System.out.println(aa.solution(125));
     }
 }
