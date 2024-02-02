e04acf92ceea2280118f74a6ddf5eef97f680288
YangHyeGyo <gpry4618@naver.com>
2024-02-02 09:17:58 +0900

    문자열 내 마음대로 정렬하기

---
+++ src/coding/test/Lv1/문자열_내_마음대로_정렬하기.java
@@ -0,0 +1,29 @@
+package coding.test.Lv1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class 문자열_내_마음대로_정렬하기 {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+        List<String> list = new ArrayList<>();
+
+        for(int i = 0; i < strings.length; i++) {
+            list.add(strings[i].charAt(n) + strings[i]);
+        }
+
+        Collections.sort(list);
+
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i).substring(1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        문자열_내_마음대로_정렬하기 aa = new 문자열_내_마음대로_정렬하기();
+        System.out.println(aa.solution(new String[]{"sun", "bed", "car"}, 1));
+    }
+}
