181c287763c75c840fd309d45e56270a50aa636d
YangHyeGyo <gpry4618@naver.com>
2024-02-05 09:00:28 +0900

    문자열 내 마음대로 정렬하기2

--- src/coding/test/Lv1/문자열_내_마음대로_정렬하기.java
+++ src/coding/test/Lv1/문자열_내_마음대로_정렬하기.java
@@ -9,13 +9,13 @@ public class 문자열_내_마음대로_정렬하기 {
         String[] answer = new String[strings.length];
         List<String> list = new ArrayList<>();
 
-        for(int i = 0; i < strings.length; i++) {
+        for(int i=0; i<strings.length; i++){
             list.add(strings[i].charAt(n) + strings[i]);
         }
 
         Collections.sort(list);
 
-        for(int i = 0; i < list.size(); i++) {
+        for(int i=0; i<strings.length; i++){
             answer[i] = list.get(i).substring(1);
         }
 
