cd239fa5d19826f9d5565ccbd806376a58dce7ce
YangHyeGyo <gpry4618@naver.com>
2024-02-07 13:39:15 +0900

    올바른 괄호 스택 사용

--- src/coding/test/Lv2/올바른_괄호.java
+++ src/coding/test/Lv2/올바른_괄호.java
@@ -1,5 +1,7 @@
 package coding.test.Lv2;
 
+import java.util.Stack;
+
 public class 올바른_괄호 {
     boolean solution(String s) {
         int cnt1 = 0, cnt2 = 0;
@@ -21,11 +23,34 @@ public class 올바른_괄호 {
          return false;
     }
 
+    boolean solution1(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0 ;i<s.length(); i++){
+
+            if(s.charAt(i) == '('){
+                stack.push('(');
+            } else if (s.charAt(i) == ')') {
+                if(stack.empty()){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
     public static void main(String[] args) {
         올바른_괄호 aa = new 올바른_괄호();
         System.out.println(aa.solution("()()"));
         System.out.println(aa.solution("(())()"));
         System.out.println(aa.solution(")()("));
         System.out.println(aa.solution("(()("));
+
+        System.out.println(aa.solution1("()()"));
+        System.out.println(aa.solution1("(())()"));
+        System.out.println(aa.solution1(")()("));
+        System.out.println(aa.solution1("(()("));
+
     }
 }
