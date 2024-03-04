3f81c5326b9254479e074a5d65e483bbf5a16edf
YangHyeGyo <gpry4618@naver.com>
2024-03-04 09:13:15 +0900

    수열과 구간 쿼리 4_2

---
+++ src/coding/test/Lv0/수열과_구간_쿼리_4_2.java
@@ -0,0 +1,13 @@
+package coding.test.Lv0;
+
+public class 수열과_구간_쿼리_4_2 {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        수열과_구간_쿼리_4_2 aa = new 수열과_구간_쿼리_4_2();
+        System.out.println(aa.solution(new int[]{0, 1, 2, 4, 3}, new int[][]{{0,4,1}, {0,3,2}, {0,3,3}}));
+    }
+}
