380cd5f6050b8af20fe1d0a300dd5362362d7954
YangHyeGyo <gpry4618@naver.com>
2024-03-04 10:24:07 +0900

    수열과 구간 쿼리 4_2

--- src/coding/test/Lv0/수열과_구간_쿼리_4_2.java
+++ src/coding/test/Lv0/수열과_구간_쿼리_4_2.java
@@ -2,8 +2,15 @@ package coding.test.Lv0;
 
 public class 수열과_구간_쿼리_4_2 {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        return answer;
+        for(int i=0; i<queries.length; i++){
+            for(int j=queries[i][0]; j<=queries[i][1]; j++){
+                if(j % queries[i][2] == 0){
+                    arr[j]++;
+                }
+            }
+        }
+
+        return arr;
     }
 
     public static void main(String[] args) {
